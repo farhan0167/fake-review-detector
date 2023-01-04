b9bb2d3ca315ebed5f7389c70a9b1b4b7a4c8505
farhan0167 <afi225@nyu.edu>
2023-01-04 16:37:06 -0500

    updated codebase-added ratelimiter

--- backend/app.py
+++ backend/app.py
@@ -24,8 +24,9 @@ def welcome():
         "message": "Hello"
     }
 
-@limiter.limit("2/minute")
+
 @app.route("/get-connection-info")
+@limiter.limit("10/minute")
 def connecttion_info():
     server_name = socket.gethostname()
     server_ip = socket.gethostbyname(server_name)
